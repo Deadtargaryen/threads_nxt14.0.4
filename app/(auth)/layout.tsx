28892506e81713c8d65be1b04d26ccab1ba5be2b
Deadtargaryen <mannyetukudoh@gmail.com>
2023-12-24 14:20:47 +0100

    docs understood

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,4 +1,5 @@
-import { ClerkProvider } from '@clerk/nextjs'
+import { ClerkProvider, SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import Head from "next/head"; // Import the Head component from next/head
 import { Inter } from 'next/font/google'
 
 import '../globals.css'
@@ -8,20 +9,38 @@ export const metadata = {
     description: 'Threads next14 v1.00 application'
 }
 
-const inter = Inter ({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] })
 
-export default function RootLayout({ 
-    children
- } : {
-    children: React.ReactNode
-}) {
-    return (
-    <ClerkProvider>
-        <html lang='en'>
-        <body className={`${inter.className} bg-dark-1`}>
-            {}
-        </body>
-        </html>
-    </ClerkProvider>
-    )
+function Header() {
+  return (
+    <header style={{ display: "flex", justifyContent: "space-between", padding: 20 }}>
+      <h1>Threads</h1>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+    </header>
+  );
+}
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang='en'>
+      <Head>
+        {/* Set HTML lang attribute and include metadata */}
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+      </Head>
+      <body className={`${inter.className} bg-dark-1`}>
+        <ClerkProvider>
+          <Header />
+          {children}
+        </ClerkProvider>
+      </body>
+    </html>
+  );
 }
