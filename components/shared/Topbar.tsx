5918173ce04aec195e1b60fe8b1580051e3ed15e
Deadtargaryen <mannyetukudoh@gmail.com>
2023-12-26 02:12:00 +0100

    updated layout.tsx, LeftSidebar.tsx, & created constants

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,8 +1,10 @@
+import { ClerkProvider, OrganizationSwitcher, SignedIn, SignedOut } from "@clerk/nextjs"
 import Image from "next/image"
 import Link from "next/link"
 
 function Topbar() {
     return (
+        <ClerkProvider>
     <nav className="topbar">
         <Link href='/' className="flex items-center gap-4">
             <Image src="/logo.svg" alt="logo" width={28} height={28} />
@@ -10,7 +12,34 @@ function Topbar() {
                 Threads
             </p>
         </Link>
+
+        <div className="flex items-center gap-1">
+            <div className="block md:hidden">
+                <SignedIn>
+                    <SignedOut>
+                        <div className="flex cursor-pointer">
+                            <Image 
+                            src="/assets/logout.svg"
+                            alt="logout"
+                            width={24}
+                            height={24}
+                            />
+                        </div>
+                    </SignedOut>
+                </SignedIn>
+            </div>
+
+            <OrganizationSwitcher
+            appearance={{
+                elements: {
+                    organizationSwitcherTrigger: 
+                    "py-2 px-4"
+                }
+            }}
+            />
+        </div>
     </nav>
+    </ClerkProvider>
     )
 }
 
